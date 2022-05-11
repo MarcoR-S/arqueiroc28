07fe23fbd1518182831f7585926eebf574b80f85
MarcoR-S <95389087+MarcoR-S@users.noreply.github.com>
2022-05-10 23:37:51 -0300

    Update sketch.js

--- PRO-V2-C28-Modelo-main/sketch.js
+++ PRO-V2-C28-Modelo-main/sketch.js
@@ -69,9 +69,9 @@ function draw() {
         score += 5;
       }*/
 
-      /*if (board1Collision.collided || board2Collision.collided) {
+      if (board1Collision.collided || board2Collision.collided) {
         score += 5;
-      }*/
+      }
 
       /*if (board1Collision.collided || board2Collision.collided) {
         score = 5;
